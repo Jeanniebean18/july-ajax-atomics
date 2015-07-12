b7586f07f1c89bfef3d55fae251dd7f3adc90396
Jeannie <jeannie@kickpunchcreative.com>
2015-07-12 13:17:18 -0500

    03

--- 03/public/global.js
+++ 03/public/global.js
@@ -1,23 +1,26 @@
-var req = new XMLHttpRequest();
-
-req.open("get", "/path1");
-
-req.addEventListener("load", function(){
-  alert(this.response);
-});
-
-req.send();
-
-//  ---------------------------------------------------------------------------
-
 var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
+  var link = this;
+  var req = new XMLHttpRequest();
+
+  req.open("get", link.getAttribute("href"));
+
+  req.addEventListener("load", function(){
+    link.innerHTML = req.response;
+  });
+
+  req.send();
   // ?????????
   
   // Prevent the link from refreshing the DOM.
   event.preventDefault();
 }
 
-// When the link is clicked, run the above code.
 myLink.addEventListener("click", handleTheClick);
+
+// When the link is clicked, run the above code.
+
+
+
+
