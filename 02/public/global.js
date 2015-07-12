c998b1da18fa3c38717ef8659ce4450e1aab8f22
Jeannie <jeannie@kickpunchcreative.com>
2015-07-12 12:36:08 -0500

    done with two

--- 02/public/global.js
+++ 02/public/global.js
@@ -1,15 +1,20 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/path1");
+req.open("get", "/path2");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
 });
 
 req.addEventListener("load", function(){
-  alert(this.response);
- 
+  // alert(this.response);
+  if (this.response === "yes") {
+    alert("yay!");
+  }
+  else {
+    alert("Aw, shucks!");
+  }
 });
 
 req.send();
