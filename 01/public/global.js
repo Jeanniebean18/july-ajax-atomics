a4c7876343ef00cbac63e81a174533fa2360c64c
Jeannie <jeannie@kickpunchcreative.com>
2015-07-12 11:48:40 -0500

    git changes :)

---
+++ 01/public/global.js
@@ -0,0 +1,21 @@
+var req = new XMLHttpRequest();
+
+// For 'Exercise 2', you will modify this line:
+req.open("get", "/info2");
+
+req.addEventListener("loadstart", function(){
+  console.log("loadstart");
+});
+
+req.addEventListener("load", function(){
+  if (this.response === "yes") {
+    alert("yay!");
+  }
+  else {
+    alert("Aw, shucks!");
+  }
+    
+});
+
+req.send();
+// dfdf
